6c1a7008538c839585ec396a97fe4668fc9c15ab
Thercístenes <thercistenes1@outlook.com>
2024-03-03 18:05:48 -0300

    popula tabela com seedres

--- src/seeders/20240303185116-addresses.js
+++ src/seeders/20240303185116-addresses.js
@@ -1,44 +1,47 @@
+// src/seeders/[timestamp]-addresses.js
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  up: async (queryInterface, _Sequelize) => {
     return queryInterface.bulkInsert('addresses',
-    [
-      {
-        city: 'Belo Horizonte',
-        street: 'Rua Flórida',
-        number: 1080,
-        employee_id: 1,
-      },
-      {
-        city: 'São Paulo',
-        street: 'Avenida Paulista',
-        number: 1980,
-        employee_id: 2,
-      },
-      {
-        city: 'Fortaleza',
-        street: 'Rua das Enseadas',
-        number: 95,
-        employee_id: 3,
-      },
-      {
-        city: 'Belo Horizonte',
-        street: 'Rua Andaluzita',
-        number: 131,
-        employee_id: 4,
-      },
-      {
-        city: 'Curitiba',
-        street: 'Rua Fria',
-        number: 101,
-        employee_id: 4,
-      },
-    ], {});
+      [
+        {
+          city: 'Belo Horizonte',
+          street: 'Rua Flórida',
+          number: 1080,
+          employee_id: 1,
+        },
+        {
+          city: 'São Paulo',
+          street: 'Avenida Paulista',
+          number: 1980,
+          employee_id: 2,
+        },
+        {
+          city: 'Fortaleza',
+          street: 'Rua das Enseadas',
+          number: 95,
+          employee_id: 3,
+        },
+        {
+          city: 'Belo Horizonte',
+          street: 'Rua Andaluzita',
+          number: 131,
+          employee_id: 4,
+        },
+        {
+          city: 'Curitiba',
+          street: 'Rua Fria',
+          number: 101,
+          employee_id: 4,
+        }
+      ],
+      {},
+    );
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface, _Sequelize) => {
     return queryInterface.bulkDelete('addresses', null, {});
-  }
+  },
 };
